45ead252a1b5a15a14380dc27015163808714464
Kalebu <isaackeinstein@gmail.com>
2021-04-27 02:53:26 +0300

    twowaysms-get-balance

--- BeemAfrica/Bpay.py
+++ BeemAfrica/Bpay.py
@@ -2,10 +2,24 @@ import sys
 import requests
 from BeemAfrica import secured, get_header
 
+BPAY_BALANCE_URL = 'https://apitopup.beem.africa/v1/credit-balance?app_name={}'
+
 
 class Bpay(object):
     def __init__(self) -> None:
         super().__init__()
+
+    @secured
+    def get_balance(self, app_name='USSD'):
+        try:
+            return requests.get(
+                BPAY_BALANCE_URL.format(app_name),
+                headers=get_header()
+            ).json()
+
+        except (requests.ConnectionError, requests.ConnectTimeout):
+            raise ConnectionError(
+                'Connection to the USSD Get balance API Refused, Please check your internet connections')
 
 
 sys.modules[__name__] = Bpay()
--- BeemAfrica/TwowaySMS.py
+++ BeemAfrica/TwowaySMS.py
@@ -2,10 +2,24 @@ import sys
 import requests
 from BeemAfrica import secured, get_header
 
+TWOWAYSMS_BALANCE_URL = 'https://www.blsmsgw.com/portal/api/userAccountBalance'
+
 
 class TwowaySMS(object):
     def __init__(self) -> None:
         super().__init__()
+
+    @secured
+    def get_balance(self):
+        try:
+            return requests.get(
+                TWOWAYSMS_BALANCE_URL,
+                headers=get_header()
+            ).json()
+
+        except (requests.ConnectionError, requests.ConnectTimeout):
+            raise ConnectionError(
+                'Connection to the USSD Get balance API Refused, Please check your internet connections')
 
 
 sys.modules[__name__] = TwowaySMS()
