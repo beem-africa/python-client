866bb044d55ce844ed98df9d491e18401d04bbf1
Kalebu <isaackeinstein@gmail.com>
2021-04-27 02:46:58 +0300

    initialized twowaysms

---
+++ BeemAfrica/TwowaySMS.py
@@ -0,0 +1,11 @@
+import sys
+import requests
+from BeemAfrica import secured, get_header
+
+
+class TwowaySMS(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+sys.modules[__name__] = TwowaySMS()
